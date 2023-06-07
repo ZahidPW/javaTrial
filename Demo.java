1a0db7a8bfad9c85db428f093befdfbf86eb120f
ZahidPW <mdzahidalam907860@gmail.com>
2023-06-07 14:40:33 +0530

    first commit

--- Demo.java
+++ Demo.java
@@ -5,6 +5,11 @@ class Demo
    System.out.println(" hello zahid");
    System.out.println( args[0] );
  System.out.println( args[1] );
+ int i=0;
+ while(i<7)
+{ 
+  System.out.println("*");
+   i++;
 
-   }
+   } 
 }
